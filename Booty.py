78cf01a2ed42d897916c7ecd2fbc12f2cf60c9c5
Soho36 <vladimir.sehirev@gmail.com>
2024-01-25 01:02:53 +0200

    2

--- Booty.py
+++ Booty.py
@@ -4,17 +4,40 @@ TOKEN = '6880602647:AAEJFBAN2jnOblSJoing7AXjzZcqFCFQFNI'
 
 bot = telebot.TeleBot(TOKEN)
 
+keys = {
+    'bitcoin': 'BTC',
+    'ether': 'ETH',
+    'dollar': 'USD'
+}
+
+slovar = ['usd', 'eur', 'chf']
 
 @bot.message_handler(commands=['start', 'help', 'run'])
 def start(message: telebot.types.Message):
-    print("Received /start or /help command.")
-    print(f"Username: {message.chat.username}")
-    bot.reply_to(message, f"Welcome my dear friend {message.chat.username} or whatever")
+    # print("Received /start or /help command.")
+    # print(f"Username: {message.chat.username}")
+    text = f"Welcome my dear friend {message.chat.username} or whatever"
+    bot.reply_to(message, text)
 
+
+# @bot.message_handler(commands=['values'])
+# def values(message: telebot.types.Message):
+#     text = 'Available currencies: '
+#     for key in keys.keys():
+#         text = '\n'.join((text, key))
+#     bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Available currenciese:\n' + "\n".join(slovar)
+    bot.reply_to(message, text)
 
 @bot.message_handler(content_types=['text'])
 def reply(message: telebot.types.Message):
-    bot.send_message(message.chat.id, 'Dont understand you!')
+    text = 'Dont understand you!'
+    bot.send_message(message.chat.id, text)
+
+
 
 
 bot.polling(none_stop=True)
